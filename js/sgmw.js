58b910908e89a002a26cf3a1105d8e6380fd1656
Real <369517224@qq.com>
2017-05-24 22:09:16 +0800

    加国双检测

--- js/sgmw.js
+++ js/sgmw.js
@@ -191,7 +191,9 @@ function addInfo(_name, _tel, _province, _city, _dealer) {
                     _smq.push(['custom', '17-baojun', '730millionsownernewMB-home-submitsuccess', '{' + _name + '+' + _tel + '+' + p + '+' + c + '+' + _dealer + '}']);
                 }
                 if (window.gsTracker) {
+                    alert('触发国双');
                     var orderid = leadsID;
+                    console.log(orderid);
                     gsTracker.addOrder(orderid, 1);
                     gsTracker.setEcomProperty(orderid, "1", _name);
                     gsTracker.setEcomProperty(orderid, "2", _tel);
@@ -200,7 +202,6 @@ function addInfo(_name, _tel, _province, _city, _dealer) {
                     gsTracker.setEcomProperty(orderid, "5", c);
                     gsTracker.setEcomProperty(orderid, "6", _dealer);
                     gsTracker.addProduct(orderid, location.pathname, location.pathname, 1, 1, "全新宝骏730");
-                    //此处可以根据活动页面实际title进行更换；
                     gsTracker.trackECom();
                     gsTracker.track("/targetpage/formsubmit/sqtywlpc");
                 }
